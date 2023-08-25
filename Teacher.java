a79dc5b1b93893d3ff22622d1da5ce58b6897f0a
Tolgay Berk <ttb.explorer@gmail.com>
2023-08-25 21:00:10 +0300

    Teacher

---
+++ Teacher.java
@@ -0,0 +1,14 @@
+public class Teacher {
+    String name;
+    String branch;
+    String mpnu;
+
+    public Teacher(String name, String branch, String mpnu) {
+        this.name = name;
+        this.branch = branch;
+        this.mpnu = mpnu;
+
+    }
+
+}
+
